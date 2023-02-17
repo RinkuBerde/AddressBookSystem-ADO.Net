642f5625b4a6222af33ddd0da07c5267a55db528
rinkuberde <rinkuberde@gmail.com>
2023-02-17 16:35:41 +0530

    [Rinku] -Create Database

---
+++ AddressBookSystem-ADO.Net/AddressBookRespitory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_ADO.Net
+{
+    class AddressBookRespitory
+    {
+        //Give path for Database Connection
+        public static string connection = @"Server=(localdb)\MSSQLLocalDB;Database=Address_Book;Trusted_Connection=True;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+}
