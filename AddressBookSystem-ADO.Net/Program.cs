25e076ba3d21427443cde2ab3badad7935041166
rinkuberde <rinkuberde@gmail.com>
2023-02-17 16:24:42 +0530

    [Rinku] -Welcome Message

---
+++ AddressBookSystem-ADO.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystem_ADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBookSystem-ADO.Net");
+        }
+    }
+}
